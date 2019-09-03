d1fbd9e8623ae77276ff31e9c0dd9b212fd5272e
John Collins <j.k.collins410@gmail.com>
2019-09-03 17:39:26 -0400

    Removed unnecessary components, removed props from constructor, updated state to include a reference to pre-loaded images, removed updateState function. Fixed a bug updating the H2 on input change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import {
   BrowserRouter,
   Route,
   Switch,
-  Redirect, NavLink
+  Redirect
 } from 'react-router-dom';
 
 //App components
@@ -16,26 +16,25 @@ import NotFound from './components/NotFound';
 
 export default class App extends PureComponent {
 
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
           this.state = {
             loading: true,
-            searches: ['dry tortugas', 'sea turtles', 'sand crabs'],
-            query: ''
+            loadedSearch: ['dry tortugas', 'sea turtles', 'sand crabs'],
+            query: 'dry tortugas'
           }
   }
 
-  //When the page loads the initial search queries are called from the API
+  //When the page loads the initial activeSearch queries are called from the API
   //The first is displayed on the page, and the 2nd is saved for later use
   componentDidMount() {
     this.buildPhotoStates();
-    this.performSearch(`dry tortugas`);
-    return <Route exact path="/search_results" render={()=> <PhotoList data={this.state} upDateState={this.upDateState} />}/>
+    this.performSearch(this.state.query);
   }
 
-  //This saves the "button" searches in state for later retrieval
+  //This downloads the button items as "loadedSearch" in state for later retrieval
   buildPhotoStates (){
-    let searches = this.state.searches;
+    let searches = this.state.loadedSearch;
     for(let i = 0; i < searches.length; i++){
       let query = searches[i];
       searches[i] = [];
@@ -49,41 +48,36 @@ export default class App extends PureComponent {
     }
   }
 
-  //Handles the search input upon change
+  //Handles the activeSearch input upon change
   onSearchChange = (e) => {
-    this.setState ({ query: e.target.value});
-    this.performSearch(e.target.value);
+    this.setState({
+      query: e.target.value
+    });
   };
 
-  //Handles the search input upon submit
+  //Handles the activeSearch input upon submit
   handleSubmit = (e) => {
     e.preventDefault();
     this.performSearch(this.state.query);
     e.currentTarget.reset();
   };
 
-  //Trying to update state to for search functions to display properly
-  upDateState=(object, value)=>{
-    this.setState({
-      object: value
-    });
-  };
-
   //Takes input and returns photos from the Flickr API
   //API key is saved in ./config.js
   performSearch = (query) => {
       axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&safe_search=1&content_type=photo&extras=url_l,url_sq&per_page=24&page=1&format=json&nojsoncallback=1`)
+          //Response is returned in JSON as requested from the API
           .then(response => {
             this.setState({
-              photos: response.data.photos.photo,
+              activeSearch: response.data.photos.photo,
               loading: false,
+              h2: query,
               query
             })
           })
           .catch(error => {
             console.log('Error fetching and parsing data', error);
           });
-      console.log(query);
   };
 
   //Renders the page
@@ -91,18 +85,26 @@ export default class App extends PureComponent {
   return (
     <BrowserRouter>
       <div className="container">
-        <Search handleSubmit={this.handleSubmit} onSearchChange={this.onSearchChange} onSearch={this.performSearch} data={this.performSearch} />
-        <Nav data={this.state.searches}/>
+        <Search onSearchChange={this.onSearchChange} handleSubmit={this.handleSubmit}/>
+        <Nav performSearch={this.performSearch}/>
         <nav className="photo-container">
-        <h2> {this.state.query} </h2>
-        <Switch>
-          <Route exact path="/" render={() =><Redirect to="/dry_tortugas"/> }/>
-          <Route path="/dry_tortugas" render={()=> <PhotoList data={this.state}  upDateState={this.upDateState} search={0}/>}/>
-          <Route path="/sea_turtles" render={()=> <PhotoList data={this.state} upDateState={this.upDateState} search={1}/>}/>
-          <Route path="/sand_crabs" render={()=> <PhotoList data={this.state} upDateState={this.upDateState} search={2}/>}/>
-          <Route path="/search" render={()=> <Search data={this.state} />} />
-          <Route component={NotFound} />
-        </Switch>
+        {
+          (this.state.loading)
+              ? <h2>Dry Tortugas</h2>
+              : <h2>{this.state.h2} Photos</h2>
+        }
+          {
+            (this.state.loading)
+            ? <p>Loading.....</p>
+            : <Switch>
+            <Route exact path="/" render={() => <Redirect to="/dry tortugas"/>} />
+            <Route path="/dry tortugas" render={()=> <PhotoList data={this.state} search={0}/>}/>
+            <Route path="/sea turtles" render={()=> <PhotoList data={this.state} search={1}/>}/>
+            <Route path="/sand crabs" render={()=> <PhotoList data={this.state} search={2}/>}/>
+            <Route path="/search" render={()=> <PhotoList data={this.state}/>} />
+            <Route component={NotFound} />
+            </Switch>
+          }
       </nav>
       </div>
     </BrowserRouter>
