6ffd7c75dba0ec95f517c71bdab4616f0be79597
John Collins <j.k.collins410@gmail.com>
2019-09-03 17:31:54 -0400

    added comments and updated logic to handle input from the search field

--- my-app/src/components/PhotoList.js
+++ my-app/src/components/PhotoList.js
@@ -3,22 +3,22 @@ import Photo from './Photo';
 
 //Generates the list of photos received as an array
 const PhotoList = props => {
-  let results = props.data;
+  let results = props.data.activeSearch;
   let photos;
+
+  //Retrieves the url data within props
   function getUrl(data) {
     return (data.url_l
      ? data.url_l
      : data.url_sq);
   }
-    if (results.length > 3) {
-      console.log(results);
-      photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
-    } else if ((!props.data.loading) && (props.search >= 0)) {
-      let value = props.search;
-      props.upDateState('query', props.data.query);
-      results = props.data.searches[value][0];
-      photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
-    }
+  if((!props.data.loading) && (props.data.query)){
+    photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
+  }else if ((!props.data.loading) && (props.activeSearch >= 0)) {
+    let value = props.activeSearch;
+    results = props.data.loadedSearch[value][0];
+    photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
+  }
 
   return (
       <ul>
