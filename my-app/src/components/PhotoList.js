d931cc4d15d48505c2149b127484e72fc422b6ad
John Collins <j.k.collins410@gmail.com>
2019-09-02 20:00:26 -0400

    Updated PhotoList.js -added logic to handle viewing data downloaded after page load

--- my-app/src/components/PhotoList.js
+++ my-app/src/components/PhotoList.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Photo from './Photo';
-import NotFound from './NotFound';
 
+//Generates the list of photos received as an array
 const PhotoList = props => {
-  const results = props.data;
+  let results = props.data;
   let photos;
-  function getUrl (data) { return (data.url_l
-        ? data.url_l
-        : data.url_sq);
+  function getUrl(data) {
+    return (data.url_l
+     ? data.url_l
+     : data.url_sq);
   }
-  if (results.length > 0) {
-    photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/> );
-  } else {
-   photos = <NotFound/>;
-  }
+    if (results.length > 3) {
+      console.log(results);
+      photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
+    } else if ((!props.data.loading) && (props.search >= 0)) {
+      let value = props.search;
+      props.upDateState('query', props.data.query);
+      results = props.data.searches[value][0];
+      photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
+    }
 
   return (
       <ul>
