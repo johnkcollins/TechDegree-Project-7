b0fef929619b8d3292ba036da4622c7559453a1b
John Collins <j.k.collins410@gmail.com>
2019-09-04 19:01:56 -0400

    removed reference to props.resetRedirect() which has been removed

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -5,13 +5,12 @@ const Nav = (props) => {
       return (
         <nav className="main-nav">
           <ul>
-              <li><NavLink onClick={() => props.performSearch('Dry Tortugas') && props.resetRedirect()} exact to={`/dry tortugas`} >Dry Tortugas</NavLink></li>
-              <li><NavLink onClick={() => props.performSearch('sea turtles') && props.resetRedirect()} exact to={`/sea turtles`} >Sea Turtles</NavLink></li>
-              <li><NavLink onClick={() => props.performSearch('sand crabs') && props.resetRedirect()} exact to={`/sand crabs`} >Sand Crabs</NavLink></li>
+              <li><NavLink onClick={() => props.performSearch('Dry Tortugas') } exact to={`/dry tortugas`} >Dry Tortugas</NavLink></li>
+              <li><NavLink onClick={() => props.performSearch('sea turtles') } exact to={`/sea turtles`} >Sea Turtles</NavLink></li>
+              <li><NavLink onClick={() => props.performSearch('sand crabs') } exact to={`/sand crabs`} >Sand Crabs</NavLink></li>
           </ul>
         </nav>
     );
-
 };
 
 export default Nav
