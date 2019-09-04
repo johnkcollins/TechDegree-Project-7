e44f62a409d05da91e9c56668c59d6956c159337
John Collins <j.k.collins410@gmail.com>
2019-09-04 19:03:05 -0400

    Imported Redirect from react-router-dom and used it to render a no results page when there are no results to display

--- my-app/src/components/PhotoList.js
+++ my-app/src/components/PhotoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Photo from './Photo';
+import { Redirect }from 'react-router-dom';
 
 //Generates the list of photos received as an array
 const PhotoList = props => {
@@ -12,9 +13,12 @@ const PhotoList = props => {
      ? data.url_l
      : data.url_sq);
   }
-  if((!props.data.loading) && (props.data.query)){
+  console.log(props.data.activeSearch);
+  if((!props.data.loading) && (props.data.query) && (props.data.activeSearch !== null)){
     photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
-  }else if ((!props.data.loading) && (props.activeSearch >= 0)) {
+  } else if (props.data.activeSearch === null){
+    return <Redirect to="/no-search-results"/>;
+  } else if ((!props.data.loading) && (props.activeSearch >= 0)) {
     let value = props.activeSearch;
     results = props.data.loadedSearch[value][0];
     photos = results.map(photo => <Photo url={getUrl(photo)} key={photo.id}/>);
